c10d0585c08c54b6389e8418bc25cc6c1de0b287
hminaya <hminaya@gmail.com>
2014-03-27 14:48:21 -0400

    Aqui va....

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  protect_from_forgery
+
+  def index
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloRails::Application.config.secret_token = '8db96b9d0ad14f9c0c323a4f8a1646565e3b2f84a5972031cac4238f24d9b93943cfdb5d21c5167ed42c9e777bc43e6856e878f9b243e954ff73688c1e37568e'
